28d289a5f327212d0f26e76a4df36bd67e326fcb
therecluse26 <braddmagyar@gmail.com>
2019-08-23 19:12:08 -0400

    Create mocking-tag-min.js

---
+++ mocking-tag-min.js
@@ -0,0 +1,1 @@
+((v)=>{let c=(e)=>{let o=e.innerText,n="",i=o.length;for(let i=0;i<o.length;i++){n+=Math.random()>=0.5==1?o.charAt(i).toUpperCase():o.charAt(i).toLowerCase();}e.innerText=n;};document.createElement(v);let ts=document.getElementsByTagName(v);for (let i=0;i<ts.length;i++){c(ts[i])}})("mock")
